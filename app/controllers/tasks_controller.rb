20c0647ea88fe4ed5614cf82493019f856016c12
takashi miyasato <on.the.line.mt215@gmail.com>
2022-10-28 15:23:52 +0900

    ER図をjpgに変更

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,58 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if params[:back]
+      render :new
+    else
+      if @task.save
+        redirect_to tasks_path, notice: "投稿しました！"
+      else
+        render :new
+      end
+    end
+  end
+
+  def show
+    @task = TTask.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, notice:"削除しました！"
+  end
+
+  def confirm
+    @task = Task.new(task_params)
+    render :new if @task.invalid?
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
+end
