6fa629521fdd9f7c4b32f9fd1b210702d7fd6a8a
takashi miyasato <on.the.line.mt215@gmail.com>
2022-11-13 22:46:04 +0900

    テストした

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,27 @@
-User.create!(name: "user1", email: "user0@example.com", password: "testpassword", password_confirmation:"testpassword" )
-Task.create!(title: "初めて", content:"ファーストユーザ",user_id: 1)
+User.create!(
+  name: "user1",
+  email: "user0@example.com", 
+  password: "testpassword",
+  password_confirmation: "testpassword",
+  admin: true )
+
+10.times do |n|
+  User.create!(
+    name: "テスト太郎#{n + 1}",
+    email: "test#{n + 1}@gmail.com",
+    password: "1234567",
+    password_confirmation: "1234567"
+  )
+end
+
+10.times do |n|
+  Task.create!(
+    title: "タスク#{n + 1}",
+    content: "content#{n ; 1}",
+    deadline_on: DateTime.now,
+    status: rand(0..2),
+    priority: rand(0..2),
+    user_id: rand(1..10)
+  )
+  Label.create!(name: "ラベル#{n + 1}")
+end
---
+++ spec/system/label_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+RSpec.describe 'ラベル管理機能', type: :system do
+  let!(:second_user) { FactoryBot.create(:second_user)}
+  before do
+    # driven_by(selenium_chrome_headless)
+    current_user = User.find_by(email: "user2@gmail.com")
+    visit new_session_path
+    fill_in 'session_email', with: 'user2@gmail.com'
+    fill_in 'session_password', with: '2222222'
+    click_button 'ログイン'
+  end
+
+  describe 'ラベル登録機能' do
+    context 'タスク作成時にラベルを選択した場合' do
+      it 'タスクにラベルを付けられる' do
+        visit new_task_path
+        fill_in "task_title", with: "user1"
+        fill_in "task_content", with: "user1のやつ"
+        fill_in 'task[deadline_on]', with: "2022-11-01"
+        select '完了', from: 'task_status'
+        select '高', from: 'task_priority'
+        check 'ラベル'
+        click_button '登録する'
+        expect(page).to have_content 'ラベル'
+      end
+    end
+    it 'タスクに複数のラベルを登録できる' do
+      visit new_task_path
+      fill_in "task_title", with: "user2"
+      fill_in "task_content", with: "user2のやつ"
+      fill_in 'task[deadline_on]', with: "2022-11-02"
+      select '完了', from: 'task_status'
+      select '高', from: 'task_priority'
+      check 'label1'
+      check 'label2'
+      click_button '登録する'
+      expect(page).to have_content 'ラベル'
+    end
+  end
+end
